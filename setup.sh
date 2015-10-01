389aa6158806bfc452e682aceeea3eee642354cf
Junhua Mao <mjhustc@gmail.com>
2015-10-01 07:22:40 -0700

    Add folders

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 # download and extract images
+mkdir ./images
 wget -O ./images/NVC_images.tar.gz https://www.stat.ucla.edu/~junhua.mao/projects/child_learning_folder/NVC_images.tar.gz
 cd ./images
 tar -xzf NVC_images.tar.gz
 cd ..
 
 # download pre-calculated VggNet features
+mkdir ./cache
 wget -O ./cache/NVC_v201509_image_feat_VGGnet.npy https://www.stat.ucla.edu/~junhua.mao/projects/child_learning_folder/NVC_v201509_image_feat_VGGnet.npy
